ba7f86243ada068507d629e43c08ea71cd6b7e70
fkonkol <kaenel1337@gmail.com>
2023-03-23 23:05:36 +0100

    feat: implement getting brightest and darkest pixel values

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define WIDTH 4
-#define HEIGHT 4
+#define WIDTH 320
+#define HEIGHT 200
 
 typedef uint8_t img_t[HEIGHT][WIDTH];
 
@@ -25,11 +25,31 @@ void img_load_from_file(img_t img, const char *file_path)
     fclose(f);
 }
 
+void img_get_brightest_and_darkest_px(img_t img, uint8_t *bright, uint8_t *dark)
+{
+    uint8_t brightest = 0, darkest = 255;
+
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            if (img[y][x] < darkest) darkest = img[y][x];
+            if (img[y][x] > brightest) brightest = img[y][x];
+        }
+    }
+
+    *bright = brightest;
+    *dark = darkest;
+}
+
 static img_t image;
 
 int main(void)
 {
-    img_load_from_file(image, "danee.txt");
+    img_load_from_file(image, "przyklad.txt");
+
+    uint8_t brightest, darkest;
+    img_get_brightest_and_darkest_px(image, &brightest, &darkest);
+
+    printf("%hhu %hhu\n", brightest, darkest);
 
     return 0;
 }
